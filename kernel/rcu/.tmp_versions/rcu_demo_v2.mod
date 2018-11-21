cdc5f3a1cd6f78541c3851bd69679e7a8ad18a83
huiwq1990 <huiwq1990@163.com>
2018-11-21 15:33:26 +0000

    ew

---
+++ kernel/rcu/.tmp_versions/rcu_demo_v2.mod
@@ -0,0 +1,2 @@
+/home/vagrant/Github/linux_action/kernel/rcu/rcu_demo_v2.ko
+/home/vagrant/Github/linux_action/kernel/rcu/rcu_demo_v2.o
---
+++ kernel/rcu/rcu_demo_v2.mod.c
@@ -0,0 +1,40 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0xa2f7d132, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0xfe47ae29, __VMLINUX_SYMBOL_STR(kthread_stop) },
+	{ 0x77daf871, __VMLINUX_SYMBOL_STR(wake_up_process) },
+	{ 0xf263b82d, __VMLINUX_SYMBOL_STR(kthread_create_on_node) },
+	{ 0xb3f7646e, __VMLINUX_SYMBOL_STR(kthread_should_stop) },
+	{ 0xf9a482f9, __VMLINUX_SYMBOL_STR(msleep) },
+	{ 0x643e0ce5, __VMLINUX_SYMBOL_STR(call_rcu_sched) },
+	{ 0x85d4c68, __VMLINUX_SYMBOL_STR(kmem_cache_alloc_trace) },
+	{ 0xe9423d3f, __VMLINUX_SYMBOL_STR(kmalloc_caches) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+	{ 0x37a0cba, __VMLINUX_SYMBOL_STR(kfree) },
+	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+
+MODULE_INFO(srcversion, "DEA0720C5F91AE70453A33E");
